7a96897d2db02ffb59efef1fa049adbe2660bcba
Markus Brunner <mbrunner@united-internet.de>
2025-08-29 11:54:28 +0200

    updates

--- bilder/bilder.php
+++ bilder/bilder.php
@@ -1,22 +1,18 @@
-<?php include '../../include/head.php'; ?>
-	
 <h1>Bilder</h1>
 
-	<h2><a href="hintergruende/hintergruende.php">Hintergr&uuml;nde</a></h2>
+	<h2><a href="hintergruende/hintergruende.html">Hintergr&uuml;nde</a></h2>
 	<p class="inhalt1">
 		Hier gibts Hintergr&uuml;nde f&uuml;r Deinen PC.
 	</p>
-	<h2><a href="auftritte/auftritte.php">Auftritte</a></h2>
+	<h2><a href="auftritte/auftritte.html">Auftritte</a></h2>
 	<p class="inhalt1">
 		Hier findest Du alle Auftrittsbilder.
 	</p>
-	<h2><a href="promotion/promotion.php">Promotion</a></h2>
+	<h2><a href="promotion/promotion.html">Promotion</a></h2>
 	<p class="inhalt1">
 		Hier findest Du die Promotion-Bilder von Dark Rain.
 	</p>
-	<h2><a href="studio/studio.php">Studio</a></h2>
+	<h2><a href="studio/studio.html">Studio</a></h2>
 	<p class="inhalt1">
 		Hier sind Bilder, die bei Aufnahmen von Alben im Studio aufgenommen wurden.
 	</p>
-	
-<?php include '../../include/footer.php'; ?>
--- bilder/hintergruende/hintergruende.php
+++
@@ -1,8 +0,0 @@
-<?php include '../../../include/head.php'; ?>
-	
-<h1>Hintergr&uuml;nde</h1>
-
-<p>Es stehen leider noch keine Dateien zur Auswahl.</p>
-
-
-<?php include '../../../include/footer.php'; ?>
